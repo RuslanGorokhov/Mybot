b4bbbf936b3bc5034946963be589b8861bd8d1eb
RuslanGorokhov <89922736+RuslanGorokhov@users.noreply.github.com>
2021-09-27 18:43:09 +0300

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route("/", methods=["GET", "POST"])
-def receive_update():
-    if request.method == "POST":
-        print(request.json)
-    return {"ok": True}
