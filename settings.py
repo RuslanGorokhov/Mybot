16d01263c2c4a4de6962f3046b642acbdd603990
RuslanGorokhov <89922736+RuslanGorokhov@users.noreply.github.com>
2021-09-28 21:52:02 +0300

    Delete settings.py

--- settings.py
+++
@@ -1,5 +0,0 @@
-API_KEY = '1987622231:AAEox9xrOUNDctvdaqtnkojqulB_HQUzZg0'
-proxy_url = 'socks5h://t2.learn.python.ru:1080'
-PROXY_USERNAME = 'learn'
-PROXY_PASSWORD = 'python'
-USER_EMOJI = [':smiley_cat:', ':smiling_imp:', ':panda_face:', ':dog:',':earth_asia:', ':tada:']
